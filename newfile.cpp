7b1d4e6d74e17cd374e04ddde566125171c4e144
Stevedabatman <lwinnandakyaw@gmail>
2024-08-27 14:24:18 +0630

    mote sein lar

---
+++ newfile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int numbers[] = {1, 2, 3, 4, 5};
+    int size = 5;
+
+    for (int i = 0; i <= size; ++i) 
+    {
+        cout << "Number: " << numbers[i] << endl;
+    }
+
+    return 0;
+}
